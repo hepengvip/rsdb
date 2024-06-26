86ca8cf29a28e6b668b4207d9bd10d95ddd39d25
贺心鹏 <hexinpeng@liquidnetwork.com>
2024-06-26 21:24:25 +0800

    add: packer impl

--- packet/src/lib.rs
+++ packet/src/lib.rs
@@ -1,30 +1,35 @@
-use std::io::{BufRead};
+use std::io::BufRead;
+
+use byteorder::{BigEndian, ReadBytesExt};
+
+// length constants
+pub const CMD_LENGTH: usize = 1;
+pub const LEN_LENGTH: usize = 2;
+pub const TOKEN_LENGTH: usize = 4;
+
+// commands
+const CMD_WRITE: u8 = 0x01;
+const CMD_DELETE: u8 = 0x02;
+const CMD_READ: u8 = 0x03;
+
+// responses
+const RESP_OK: u8 = 0x55;
+const RESP_ERROR: u8 = 0x56;
+const RESP_TOKEN: u8 = 0x57;
+const RESP_TOKENS: u8 = 0x58;
+const RESP_PAIRS: u8 = 0x59;
 
 
-const CMD_LENGTH: usize = 1;
-const LEN_LENGTH: usize = 2;
-const TOKEN_LENGTH: usize = 4;
-
-const CMD_IDCODE : u8 = 0x01;
-const CMD_WRITE : u8 = 0x02;
-const CMD_READ  : u8 = 0x03;
-const CMD_DELETE: u8 = 0x04;
-const CMD_RESP_OK: u8 = 0x55;
-const CMD_RESP_ERROR: u8 = 0x56;
-const CMD_RESP_LIST: u8 = 0x57;
-
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
+#[derive(Debug, PartialEq)]
 pub enum Packet {
-    Write(Vec<Vec<u8>>),
-    Read(Vec<Vec<u8>>),
-    Delete(Vec<Vec<u8>>),
-    RespOk,
+    CmdWrite(Vec<Vec<u8>>),
+    CmdRead(Vec<Vec<u8>>),
+    CmdDelete(Vec<Vec<u8>>),
+    RespOk(String),
     RespError(String),
-    RespList(Vec<Vec<u8>>),
+    RespToken(Vec<u8>),
+    RespTokens(Vec<Vec<u8>>),
+    RespPairs(Vec<Vec<u8>>),
 }
 
 pub struct Packer<T: BufRead> {
@@ -36,50 +41,228 @@ impl<T: BufRead> Packer<T> {
         Self { reader }
     }
 
-    // pub fn read_header(&mut self) -> Option<u32> {
-    //     let mut header = [0; HEADER_LENGTH as usize];
-    //     if self.reader.read_exact(&mut header).is_err() {
-    //         return None;
-    //     }
-    //     Some(u32::from_be_bytes(header))
-    // }
+    pub fn read_packet(&mut self) -> Packet {
+        let header = self.read_header();
 
-    // pub fn read_key(&mut self, length: u32) -> Option<Vec<u8>> {
-    //     let mut key = vec![0; length as usize];
-    //     if self.reader.read_exact(&mut key).is_err() {
-    //         return None;
-    //     }
-    //     Some(key)
-    // }
+        match header {
+            CMD_WRITE => {
+                let pairs = self.read_size();
+                let mut tokens = Vec::new();
+                for _ in 0..pairs {
+                    let token = self.read_token();
+                    tokens.push(token);
+                    let token = self.read_token();
+                    tokens.push(token);
+                }
+                return Packet::CmdWrite(tokens);
+            }
+            CMD_DELETE => {
+                let key_count = self.read_size();
+                let mut keys = Vec::new();
+                for _ in 0..key_count {
+                    let key = self.read_token();
+                    keys.push(key);
+                }
+                return Packet::CmdDelete(keys);
+            }
+            CMD_READ => {
+                let key_count = self.read_size();
+                let mut keys = Vec::new();
+                for _ in 0..key_count {
+                    let key = self.read_token();
+                    keys.push(key);
+                }
+                return Packet::CmdRead(keys);
+            }
+            RESP_OK => {
+                let message = self.read_token();
+                let message = String::from_utf8(message).unwrap();
+                return Packet::RespOk(message);
+            }
+            RESP_ERROR => {
+                let message = self.read_token();
+                let message = String::from_utf8(message).unwrap();
+                return Packet::RespError(message);
+            }
+            RESP_TOKEN => {
+                let token = self.read_token();
+                return Packet::RespToken(token);
+            }
+            RESP_TOKENS => {
+                let token_count = self.read_size();
+                let mut tokens = Vec::new();
+                for _ in 0..token_count {
+                    let token = self.read_token();
+                    tokens.push(token);
+                }
+                return Packet::RespTokens(tokens);
+            }
+            RESP_PAIRS => {
+                let pair_count = self.read_size();
+                let mut pairs = Vec::new();
+                for _ in 0..pair_count {
+                    let token = self.read_token();
+                    pairs.push(token);
+                    let token = self.read_token();
+                    pairs.push(token);
+                }
+                return Packet::RespPairs(pairs);
+            }
 
-    // pub fn read_value(&mut self, length: u32) -> Option<Vec<u8>> {
-    //     let mut value = vec![0; length as usize];
-    //     if self.reader.read_exact(&mut value).is_err() {
-    //         return None;
-    //     }
-    //     Some(value)
-    // }
+            _ => {
+                panic!("Unknown packet");
+            }
+        }
+    }
 
-    // pub fn read_record(&mut self) -> Option<Record> {
-    //     let length = self.read_header()?;
-    //     let key = self.read_key(length)?;
-    //     let value = self.read_value(length)?;
-    //     Some(Record::new(&key, &value))
-    // }
+    fn read_header(&mut self) -> u8 {
+        self.reader.read_u8().unwrap()
+    }
+
+    fn read_size(&mut self) -> u16 {
+        self.reader.read_u16::<BigEndian>().unwrap()
+    }
+
+    fn read_token(&mut self) -> Vec<u8> {
+        let length = self.reader.read_u32::<BigEndian>().unwrap();
+        let mut key = vec![0u8; length as usize];
+        self.reader.read_exact(&mut key).unwrap();
+        key
+    }
 }
-
-
-
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_write() {
+        let bytes = vec![
+            CMD_WRITE,  // packet type id
+            0, 2,  // pair count
+            0, 0, 0, 3, b'k', b'e', b'y',  // key 1
+            0, 0, 0, 3, b'v', b'a', b'l',  // value 1
+            0, 0, 0, 5, b'h', b'e', b'l', b'l', b'o',  // key 2
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // value 2
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::CmdWrite(vec![b"key".to_vec(), b"val".to_vec(), b"hello".to_vec(), b"world".to_vec()]),
+        );
+    }
+
+    #[test]
+    fn test_delete() {
+        let bytes = vec![
+            CMD_DELETE,  // packet type id
+            0, 2,  // pair count
+            0, 0, 0, 3, b'k', b'e', b'y',  // key 1
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // value 2
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::CmdDelete(vec![b"key".to_vec(), b"world".to_vec()]),
+        );
+    }
+
+    #[test]
+    fn test_read() {
+        let bytes = vec![
+            CMD_READ,  // packet type id
+            0, 2,  // pair count
+            0, 0, 0, 3, b'k', b'e', b'y',  // key 1
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // value 2
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::CmdRead(vec![b"key".to_vec(), b"world".to_vec()]),
+        );
+    }
+
+    #[test]
+    fn test_resp_ok() {
+        let bytes = vec![
+            RESP_OK,  // packet type id
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // message
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::RespOk("world".to_string()),
+        );
+    }
+
+    #[test]
+    fn test_resp_error() {
+        let bytes = vec![
+            RESP_ERROR,  // packet type id
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // message
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::RespError("world".to_string()),
+        );
+    }
+
+    #[test]
+    fn test_resp_token() {
+        let bytes = vec![
+            RESP_TOKEN,  // packet type id
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // token
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::RespToken(b"world".to_vec()),
+        );
+    }
+
+    #[test]
+    fn test_resp_tokens() {
+        let bytes = vec![
+            RESP_TOKENS,  // packet type id
+            0, 3,  // token count
+            0, 0, 0, 3, b'k', b'e', b'y',  // token 1
+            0, 0, 0x01, 0x1c, b't', b'h', b'i', b's', b' ', b'i', b's', b' ', b'a', b' ', b's', b'u', b'c', b'c', b'e', b's', b's', b'f', b'u', b'l', b' ', b'm', b'e', b's', b's', b'a', b'g', b'e', b' ', b'a', b'b', b'o', b'u', b't', b' ', b't', b'h', b'e', b' ', b's', b'y', b's', b't', b'e', b'm', b' ', b'j', b's', b'd', b'f', b'j', b'i', b'o', b'e', b'w', b'f', b'j', b'l', b'k', b's', b'f', b'j', b'i', b'e', b'w', b'f', b'w', b'o', b'f', b'j', b'd', b's', b'j', b'l', b's', b'k', b'f', b'j', b'k', b'l', b's', b'f', b'j', b'l', b'd', b'k', b's', b'f', b'j', b'l', b'd', b'k', b's', b'f', b'j', b'k', b'l', b's', b'f', b'j', b'd', b'k', b'l', b's', b'f', b'j', b'd', b'k', b'l', b's', b'f', b'j', b'k', b'd', b'f', b'j', b'd', b'k', b'f', b'j', b'd', b'k', b'f', b'j', b'd', b'k', b'l', b's', b'f', b'j', b'd', b'k', b's', b'f', b'j', b'd', b'k', b's', b'f', b'j', b'd', b's', b'k', b'f', b'j', b'd', b's', b'k', b'f', b'j', b'd', b's', b'f', b'j', b'd', b's', b'f', b'd', b's', b'f', b'd', b'f', b'd', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'f', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'a', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'd', b'f', b'e', b'f', b'e', b'f', b'e', b'w', b'f', b'e', b'f', b'a',  // token 2
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // token 3
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::RespTokens(vec![
+                b"key".to_vec(),
+                b"this is a successful message about the system jsdfjioewfjlksfjiewfwofjdsjlskfjklsfjldksfjldksfjklsfjdklsfjdklsfjkdfjdkfjdkfjdklsfjdksfjdksfjdskfjdskfjdsfjdsfdsfdfdfffffffffffffffffffffffffffffffffffaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadddddddddddddddddddddddddddddddddddddfefefewfefa".to_vec(),
+                b"world".to_vec(),
+            ]),
+        );
+    }
+
+    #[test]
+    fn test_resp_pairs() {
+        let bytes = vec![
+            RESP_PAIRS,  // packet type id
+            0, 2,  // pair count
+            0, 0, 0, 3, b'k', b'e', b'y',  // key 1
+            0, 0, 0, 5, b'v', b'a', b'l', b'u', b'e',  // value 1
+            0, 0, 0, 5, b'h', b'e', b'l', b'l', b'o',  // key 2
+            0, 0, 0, 5, b'w', b'o', b'r', b'l', b'd',  // value 2
+        ];
+        let mut packer = Packer::new(&bytes[..]);
+        let packet = packer.read_packet();
+        assert_eq!(
+            packet,
+            Packet::RespPairs(vec![b"key".to_vec(), b"value".to_vec(), b"hello".to_vec(), b"world".to_vec()]),
+        );
     }
 }
