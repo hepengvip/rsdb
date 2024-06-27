7fa98d7e539057f6b1b562613fed154fee6cc9ce
alex <alex@ws.so>
2024-06-28 00:23:46 +0800

    add: cs example

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,30 @@
+use std::{net::TcpStream, time::Duration};
+use std::thread;
+
+extern crate packet;
+
+use packet::{Packet, PacketReaderWriter};
+
+
 fn main() {
-    println!("Hello, world!");
+    let stream = TcpStream::connect("127.0.0.1:1935").unwrap();
+
+    let mut cnt = 0;
+    let mut rw = PacketReaderWriter::new(stream);
+    loop {
+        let p = Packet::CmdWrite(vec![
+            b"hello".to_vec(),
+            b"world".to_vec(),
+        ]);
+        rw.write_packet(&p);
+        let p = rw.read_packet();
+        cnt += 1;
+        println!("{cnt} Received response: {:?}", p);
+
+        if cnt >= 10 {
+            break;
+        }
+
+        thread::sleep(Duration::from_secs(1));
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,56 @@
+use std::io::Error;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
 extern crate packet;
 
-// use packet::Packet;
+use packet::{Packet, PacketReaderWriter};
+
 
 fn main() {
-    // let p = Packet {data : vec![1,2,3,4,5]};
-    // println!("Hello, {:?}!", p);
+    let s = Server::new();
+    s.listen_and_serve();
+}
+
+pub struct Server {
+    // addr: Option<String>,
+    listener: TcpListener,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        let server = Server {
+            // addr: Some("127.0.0.1:1935".to_string()),
+            listener: TcpListener::bind("127.0.0.1:1935").unwrap(),
+        };
+
+        server
+    }
+
+    pub fn listen_and_serve(&self) {
+        // Build a server
+        println!("Listening...");
+        for streams in self.listener.incoming() {
+            match streams {
+                Err(e) => { eprintln!("error: {}", e) },
+                Ok(stream) => {
+                    thread::spawn(move || {
+                        handler(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+                    });
+                }
+            }
+        }
+    }
+}
+
+fn handler(stream: TcpStream) -> Result<(), Error> {
+    println!("Connection from {}", stream.peer_addr()?);
+
+    let mut rw = PacketReaderWriter::new(stream);
+    loop {
+        let packet = rw.read_packet();
+        println!("Received packet: {:?}", packet);
+        let rsp = Packet::RespOk("All good.".to_string());
+        rw.write_packet(&rsp);
+    }
 }
